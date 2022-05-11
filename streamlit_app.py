e2183bc6701084ef46f6d3537af78700ff0f53f3
Sumeagar1222 <105217949+Sumeagar1222@users.noreply.github.com>
2022-05-11 19:06:06 +0530

    Update streamlit_app.py
    some error

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,4 +23,4 @@ streamlit.dataframe(fruit_to_show)
 streamlit.header('Fruityvice Fruit Advice!')
 
 fruityvice_reponse = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_reponse.json())
